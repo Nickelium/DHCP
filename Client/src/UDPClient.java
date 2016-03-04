e15a53f8e9f55361f8a3ceebf58d9ed2c80a1ff0
Nickelium <trantr.be@gmail.com>
2016-03-04 12:50:24 +0100

    partial implementation of dhcpdiscover + dhcpmessage extended

--- Client/src/UDPClient.java
+++ Client/src/UDPClient.java
@@ -1,9 +1,55 @@
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.net.NetworkInterface;
+import java.net.SocketException;
+import java.net.UnknownHostException;
+import java.nio.ByteBuffer;
 
 public class UDPClient 
 {
-	public void DHCPDiscover()
+	public InetAddress IPServer; 
+	public final int portServer = 1234;
+	public final String IPServerString = "10.33.14.246";
+	private DatagramSocket clientSocket;
+	
+	public  UDPClient()
 	{
+		try 
+		{
+			clientSocket = new DatagramSocket();
+		} 
+		catch (SocketException e) 
+		{
+			e.printStackTrace();
+		}
+	}
+	
+	public void DHCPDiscover() 
+	{
+		try 
+		{
+			DHCPMessage message = new DHCPMessage();
+			ByteBuffer b = ByteBuffer.allocate(4);
+			//hashcode 32 bit = 4 byte fills transactionID with exactly 4 byte
+			b.putInt(message.hashCode());
+			message.opCode = 1; //request
+			message.hardWareAddressLength = 6;
+			message.hopCount = 0;
+			message.transactionID = b.array();
+			message.clientIP = b.putInt(0).array();
+			message.yourIP = b.putInt(0).array();
+			
+			IPServer = InetAddress.getByName(IPServerString);
+			DatagramPacket sendingPacket = new DatagramPacket(message.retrieveBytes(), message.getLength(), IPServer, portServer);
+			clientSocket.send(sendingPacket);
 		
+		} 
+		catch (Exception e) 
+		{
+			e.printStackTrace();
+		}
 	}
 	
 	public  void DHCPRequest()
--- Server/src/DHCPMessage.java
+++ Server/src/DHCPMessage.java
@@ -1,24 +1,80 @@
+import java.util.ArrayList;
 
 public class DHCPMessage 
 {
-	public byte opCode; //Op code: 1 = bootRequest, 2 = BootReply
-    public byte hardWareType; //Hardware Address Type: 1 = 10MB ethernet
-    public byte hardWareAdressLength; //hardware address length: length of MACID
+	//For options only do the must, look up in the RFC
+	/*
+	 	DHCPMessageType in option 
+	 	
+	    DHCPDISCOVER = 1,   //a client broadcasts to locate servers
+
+	    DHCPOFFER = 2,     //a server offers an IP address to the device
+
+	    DHCPREQUEST = 3,   //client accepts offers from DHCP server
+
+	    DHCPDECLINE = 4,   //client declines the offer from this DHCP server
+
+	    DHCPACK = 5,       //server to client + committed IP address
+
+	    DHCPNAK = 6,       //server to client to state net address incorrect
+
+	    DHCPRELEASE = 7,   //graceful shutdown from client to Server
+
+	    DHCPINFORM = 8     //client to server asking for local info
+
+	*/
+	
+	public byte opCode; 
+						/*general type message:
+	 						1 = request,
+	 						2 = reply
+						*/
+    public byte hardWareType;
+    					/*Hardware Type: 
+    							1 = 10MB ethernet
+    							6 = IEE802 Network
+    							7 = ARCNET
+    							11 = Localtalk
+    							12 = Localnet
+    							14 = SMDS
+    							15 = Frame relay
+    							16 = Asynchronous Transfer mode
+    							17 = HDLC
+    							18 = Fibre channel
+    							19 = Asynchronous Transfer mode
+    							20 = Serial Line
+    							*/
+    public byte hardWareAddressLength; 
+    					/*hardware address length: length of MACID
+    					 * bv. PC_ASUS = 6
+    					 */
     public byte hopCount; //Hw options
     
     public byte[] secs = new byte[2]; //elapsed time from trying to boot (3)
     public byte[] flags = new byte[2]; //flags (3)
     
-    public byte[] transactionID = new byte[4]; //transaction id (5), 
-    public byte[] clientIP = new byte[4]; // client IP (5)
-    public byte[] yourIP = new byte[4]; // your client IP (5)
-    public byte[] serverIP = new byte[4]; // Server IP (5)
+    public byte[] transactionID = new byte[4]; 
+    					/*transaction id (5)
+    					 * to recognize the message request <-> reply 
+    					 */
+    public byte[] clientIP = new byte[4]; 
+    					/* client IP (5)
+    					 * 0 when no ip or unvalid
+    					 */
+    public byte[] yourIP = new byte[4];
+    					/* your client IP (5)
+    					 * The IP address that the server is assigning to the client.
+    					 */
+    public byte[] serverIP = new byte[4]; 
+    					/* Server IP (5)
+    					 * 
+    					 */
     public byte[] gateWayIP = new byte[4]; // relay agent IP (5)
     
     public byte[] clientHardWareAddress = new byte[16]; // Client HW address (16)
     public byte[] serverHostName = new byte[64]; // Optional server host name (64)
     public byte[] bootFileName = new byte[128]; // Boot file name (128)
-    public byte[] options; //options (rest)
+    public ArrayList<Byte> options = new ArrayList<>(); //options (rest)
     
     public final int minimalLength = 236; //bytes
     
@@ -27,13 +83,51 @@ public class DHCPMessage
     	//put parameters
     }
     
+    public DHCPMessage(byte[] buffer)
+    {
+    	//if(buffer.length < minimalLength) throw new Exception();
+    	opCode = buffer[0];
+    	hardWareType = buffer[1];
+    	hardWareAddressLength = buffer[2];
+    	hopCount = buffer[3];
+    	
+    	int j = 3;
+    	
+    	for(int i = 0; i < secs.length; i++, j++)
+    		secs[i] = buffer[i + j];
+    	for(int i = 0; i < flags.length; i++, j++)
+    		flags[i] = buffer[i+j];
+    	
+    	for(int i = 0; i < transactionID.length; i++, j++)
+    		transactionID[i] = buffer[i+j];
+    	for(int i = 0; i < clientIP.length; i++, j++)
+    		clientIP[i] = buffer[i+j];
+    	for(int i = 0; i < yourIP.length; i++, j++)
+    		yourIP[i] = buffer[i+j];
+    	for(int i = 0; i < serverIP.length; i++, j++)
+    		serverIP[i] = buffer[i+j];
+    	for(int i = 0; i < gateWayIP.length; i++, j++)
+    		gateWayIP[i] = buffer[i+j];
+    	
+    	for(int i = 0; i < clientHardWareAddress.length; i++, j++)
+    		clientHardWareAddress[i + j] = buffer[i+j];
+    	for(int i = 0; i < serverHostName.length; i++, j++)
+    		serverHostName[i] = buffer[i+j];
+    	for(int i = 0; i < bootFileName.length; i++, j++)
+    		bootFileName[i] = buffer[i+j];
+    	
+    	for(int i = 0; i < buffer.length - minimalLength; i++, j++)
+    		options.add(buffer[i+j]);
+    	
+    }
+    
     public byte[] retrieveBytes()
     {
     	byte[] toReturn = new byte[getLength()];
     	
     	toReturn[0] = opCode;
     	toReturn[1] = hardWareType;
-    	toReturn[2] = hardWareAdressLength;;
+    	toReturn[2] = hardWareAddressLength;;
     	toReturn[3] = hopCount;
     	
     	int j = 3;
@@ -61,8 +155,8 @@ public class DHCPMessage
     	for(int i = 0; i < bootFileName.length; i++, j++)
     		toReturn[i + j] = serverHostName[i];
     	
-    	for(int i = 0; i < options.length; i++, j++)
-    		toReturn[i + j] = options[i];
+    	for(int i = 0; i < options.size(); i++, j++)
+    		toReturn[i + j] = options.get(i);
     	
     	return toReturn;
     		
@@ -70,7 +164,7 @@ public class DHCPMessage
     
     public int getLength()
     {
-    	return minimalLength + options.length;
+    	return minimalLength + options.size();
     }
     
 
